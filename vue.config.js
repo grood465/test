684827eec177c58a9f244b877b0ada605bcd7b70
grood <grood465@gmail.com>
2024-11-07 16:58:26 +0900

    20241107

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../resources/static', // Build Directory
-  devServer: {
-    port: 1318,
-    proxy: 'http://localhost:1319' // Spring Boot Server
-  }
-})
